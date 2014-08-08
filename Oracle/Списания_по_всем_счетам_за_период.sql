5a2fe933b755f541b789060ba460191afc027a71
chornobay <i.o.chornobay@gmail.com>
2014-08-08 18:38:13 +0300

    MLINE-9612

---
+++ Oracle/Списания_по_всем_счетам_за_период.sql
@@ -0,0 +1,32 @@
+WITH accounts AS (
+  SELECT N_ACCOUNT_ID
+  FROM SI_SUBJ_ACCOUNTS
+  WHERE N_SUBJECT_ID    = 302
+  AND N_ACCOUNT_TYPE_ID = SYS_CONTEXT('CONST', 'ACC_TYPE_Personal')
+  AND C_ACTIVE          = 'Y')
+SELECT SUM(D.N_SUM_OUT)   SUM_OUT     -- Итого
+FROM accounts A,
+TABLE (
+        SI_USERS_PKG_S.USERS_ACC_DETALIZATION(
+                                              /* dt_D_BEGIN            => */ TO_DATE('20.11.2013 00:00:00', 'DD.MM.YYYY HH24:MI:SS'),
+                                              /* dt_D_END              => */ TO_DATE('20.01.2014 23:59:59', 'DD.MM.YYYY HH24:MI:SS'),
+                                              /* num_N_ACCOUNT_ID      => */ A.N_ACCOUNT_ID,
+                                              /* b_Payment             => */ 0,
+                                              /* b_WriteOff            => */ 1,
+                                              /* b_Overdraft           => */ 0,
+                                              /* vch_VC_TRANSACTION_ID => */ NULL)
+      ) D
+WHERE D.C_FL_TOTALS = 'N'
+AND   D.N_GOOD_ID IN (
+                        SELECT PO_P.N_GOOD_ID
+                        FROM SD_V_PRICE_ORDERS_C    PO_P,
+                             SD_V_PRICE_ORDERS_C    PO_D
+                        WHERE PO_P.N_PAR_LINE_ID IS NULL
+                        AND   PO_D.N_PAR_LINE_ID    = PO_P.N_PRICE_ORDER_LINE_ID
+                        AND   PO_D.N_GOOD_ID        = 40217401  -- Доступ в Интернет
+                        GROUP BY PO_P.N_GOOD_ID
+                        UNION ALL
+                        SELECT 139923101 N_GOOD_ID              -- Пауза
+                        FROM DUAL
+                     )
+;
